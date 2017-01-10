df0491aff378bd8059db9cb6d9b20d173a985253
Ena Markovic <ena.markovic@smartsheet.com>
2017-01-10 10:43:07 -0800

    did part 1

---
+++ lab2-part1.ts
@@ -0,0 +1,31 @@
+class Checking {
+	private ownerName:string;
+	private balance:number;	
+	
+	constructor(ownerName:string, initialBalance:number) {
+		this.balance = initialBalance;
+		this.ownerName = ownerName;
+	}
+
+	deposite(amount:number) {
+		this.balance += amount;
+	}
+
+	withdraw(amount:number) {
+		this.balance -= amount;
+	}
+
+	checkBalance() {
+		console.log(`Balanace for {this.ownerName}'s checking account: ${this.balance}`);
+	}	
+}
+
+let myChecking = new Checking('Ena', 10000);
+console.log('initial balance');
+myChecking.checkBalance();
+console.log('deposited 1000');
+myChecking.deposite(1000);
+myChecking.checkBalance();
+console.log('widthdrew 1000');
+myChecking.withdraw(1000);
+myChecking.checkBalance();
